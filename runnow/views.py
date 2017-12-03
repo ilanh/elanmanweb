6c39decc08ae4c1ce5177751beb243eeb01526fc
Ilan Harel <ilanharel@gmail.com>
2017-12-03 21:01:54 +0200

    home page

--- runnow/views.py
+++ runnow/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .forms import *
-# from myprofile.forms import *
 from formtools.wizard.views import SessionWizardView
 # from django.forms import formset_factory
 
@@ -8,7 +7,6 @@ FORMS = [("regions", RegionFormset),
          ("roles", RoleFormset),
          ("brands", BrandFormset),
          ("servernodes", ServerNodeFormset)]
-         # ("f3", ProfileFormM)]
 
 
 TEMPLATES = {"regions": "regions.html",
@@ -28,28 +26,3 @@ class AnswerWizard(SessionWizardView):
     def get_context_data(self, **kwargs):
         data = super(AnswerWizard, self).get_context_data(**kwargs)
         return data
-
-# def index(request):
-#     DrinkFormSet = formset_factory(DrinkForm, extra=2, max_num=20)
-#     if request.method == 'POST':
-#     # TODO
-#     else:
-#         formset = DrinkFormSet(initial=[{'name': 1, 'size': 'm', 'amount': 1}])
-#     return render(request, 'online/index.html', {'formset': formset})
-# def post(request):
-#         form = DistributorForm(request.POST)
-#         form.product_instances = ProductFormset(request.POST)
-#         if form.is_valid():
-#             distributor= Distributor() #model class
-#             distributor.name= form.cleaned_data('name')
-#             distributor.save()
-#             if form.product_instances.cleaned_data is not None:
-#                 for items in form.product_instances.cleaned_data:
-#                     product = Product() #Product model class
-#                     product.name= item['name']
-#                     product.quantity= item['quantity']
-#                     product.price= item['price']
-#                     product.save()
-#                     distributor.products.add(product)
-#             return redirect('/success')
-#         return redirect('/failure')
