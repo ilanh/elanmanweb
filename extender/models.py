1f8dd8ecd2a80ad9d0d39661342c07abd1caea3d
Ilan Harel <ilanharel@gmail.com>
2017-12-06 20:58:06 +0200

    dev area

--- extender/models.py
+++ extender/models.py
@@ -94,6 +94,91 @@ class LogicalGroupObject(models.Model):
         return self.shortname
 
 
+class ConfigSectionObjectQuerySet(models.QuerySet):
+    def search(self, query):
+        if query:
+            query = query.strip()
+            return self.filter(
+                Q(shortname__icontains=query) |
+                Q(shortname__iexact=query)
+            ).distinct()
+        return self
+
+
+class ConfigSectionObjectManager(models.Manager):
+    def get_queryset(self):
+        return ConfigSectionObjectQuerySet(self.model, using=self._db)
+
+    def search(self, query):
+        return self.get_queryset().search(query)
+
+
+class ConfigSectionObject(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    shortname = models.CharField(max_length=32)
+    exid = models.CharField(max_length=8, null=True)
+    desc = models.CharField(max_length=64, null=True)
+    listobject = models.BooleanField(default=False)
+    slug = models.SlugField(null=True, blank=True)
+
+    def __str__(self):
+        return self.shortname
+
+    objects = ConfigSectionObjectManager()
+
+    def get_absolute_url(self):
+        return reverse('extend:getconfigsection', kwargs={'slug': self.slug})
+
+    def get_absolute_url_edit(self):
+        return reverse('extend:editconfigsection', kwargs={'slug': self.slug})
+
+    @property
+    def title(self):
+        return self.shortname
+
+
+class RoleTaskObjectQuerySet(models.QuerySet):
+    def search(self, query):
+        if query:
+            query = query.strip()
+            return self.filter(
+                Q(shortname__icontains=query) |
+                Q(shortname__iexact=query)
+            ).distinct()
+        return self
+
+
+class RoleTaskObjectManager(models.Manager):
+    def get_queryset(self):
+        return RoleTaskObjectQuerySet(self.model, using=self._db)
+
+    def search(self, query):
+        return self.get_queryset().search(query)
+
+
+class RoleTaskObject(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    shortname = models.CharField(max_length=32)
+    desc = models.CharField(max_length=64, null=True)
+    role = models.ForeignKey(RoleObject, on_delete=models.CASCADE, null=True, blank=True)
+    slug = models.SlugField(null=True, blank=True)
+
+    def __str__(self):
+        return self.shortname
+
+    objects = RoleTaskObjectManager()
+
+    def get_absolute_url(self):
+        return reverse('extend:getroletask', kwargs={'slug': self.slug})
+
+    def get_absolute_url_edit(self):
+        return reverse('extend:editroletask', kwargs={'slug': self.slug})
+
+    @property
+    def title(self):
+        return self.shortname
+    
+    
 def ro_pre_save_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = unique_slug_generator(instance)
@@ -104,5 +189,18 @@ def lgo_pre_save_receiver(sender, instance, *args, **kwargs):
         instance.slug = unique_slug_generator(instance)
 
 
+def cs_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+
+def rt_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+
+
 pre_save.connect(ro_pre_save_receiver, sender=RoleObject)
 pre_save.connect(lgo_pre_save_receiver, sender=LogicalGroupObject)
+pre_save.connect(cs_pre_save_receiver, sender=ConfigSectionObject)
+pre_save.connect(rt_pre_save_receiver, sender=RoleTaskObject)
