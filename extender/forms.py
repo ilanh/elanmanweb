9401f3df33d15ef25a7b41684c5f93aa4732593f
Ilan Harel <ilanharel@gmail.com>
2017-12-09 15:09:56 +0200

    api templates, #5

--- extender/forms.py
+++ extender/forms.py
@@ -72,6 +72,7 @@ class ApiSectionCreateForm(forms.ModelForm):
         model = ApiSectionObject
         fields = [
             'shortname',
+            'exid',
             'api',
             'desc',
         ]
