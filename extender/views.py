9683b97386c52a3d7c9d04d98e455d680c125187
Ilan Harel <ilanharel@gmail.com>
2017-12-14 20:12:02 +0200

    github hook final test

--- extender/views.py
+++ extender/views.py
@@ -117,7 +117,7 @@ def hello(request):
     if event == 'ping':
         return HttpResponse('pong')
     elif event == 'push':
-        subprocess.call("post-receive", shell=True)
+        r = subprocess.call("post-receive")
         return HttpResponse('success')
 
     # In case we receive an event that's not ping or push
